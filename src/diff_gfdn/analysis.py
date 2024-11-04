many changes, most importantly fixed bug in SVF calculation, added test with single slope subband RIR

src/diff_gfdn/analysis.py
@@ -1,16 +1,16 @@
 from typing import List, Optional, Tuple
 
+import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
-from scipy.signal import fftconvolve
 from slope2noise.DecayFitNet.python.toolbox.DecayFitNetToolbox import DecayFitNetToolbox
 from slope2noise.DecayFitNet.python.toolbox.core import decay_model, discard_last_n_percent, PreprocessRIR
 from slope2noise.DecayFitNet.python.toolbox.utils import calc_mse
-from slope2noise.slope2noise.utils import octave_filtering
+from slope2noise.slope2noise.utils import calculate_amplitudes_least_squares, octave_filtering
 import torch
 
 from .filters.geq import octave_bands
-from .utils import db2lin, ms_to_samps
+from .utils import db, db2lin, ms_to_samps
 
 
 def get_edc_params(rir: ArrayLike,
@@ -29,11 +29,12 @@ def get_edc_params(rir: ArrayLike,
         n_slopes and n_subbands. The normalisation values and the fitted EDC per subband 
     """
     filter_frequencies = octave_bands() if use_octave_freqs else None
-    est_params_decay_net, norm_vals, fitted_edc_subband = get_decay_fit_net_params(
+    est_params_decay_net, norm_vals, fitted_edc_decayfitnet = get_decay_fit_net_params(
         rir, filter_frequencies, n_slopes, fs)
     est_t60 = est_params_decay_net[0]
     est_amp = est_params_decay_net[1]
     est_noise_level = est_params_decay_net[2]
+    fitted_edc_subband = torch.mean(fitted_edc_decayfitnet, dim=1)
     return est_t60, est_amp, est_noise_level, norm_vals, fitted_edc_subband
 
 
@@ -98,6 +99,113 @@ def get_decay_fit_net_params(
     ), fitted_edc_decayfitnet
 
 
+def get_decay_times_for_rirs(
+        rir: ArrayLike,
+        approx_rir: ArrayLike,
+        n_slopes: int,
+        fs: float,
+        band_centre_hz: ArrayLike,
+        plot_edc: bool = True,
+        mixing_time_ms: float = 20.0,
+        crop_end_ms: float = 5.0) -> Tuple[NDArray, NDArray]:
+    """
+    Compare and plot the T60s for two RIRs.
+    Args:
+        rir (ArrayLike): true (desired) RIR
+        approx_rir (ArrayLike): synthesized RIR
+        n_slopes (int): number of slopes in RIR
+        fs (float): sampling rate
+        band_centre_hz (ArrayLike): octave band frequencies in Hz
+        plot_edc (bool): whether to plot the original and fitted EDC
+        mixing_time_ms (float): truncate RIRs upto this time
+        crop_end_ms (float): ignore the last few ms of RIRs
+    Returns:
+        Tuple[NDArray, NDArray]: the T60s of shape n_slopes x n_bands 
+                                 for the original RIR and the approximated RIR
+    """
+    # crop the RIR upto the mixing time
+    mixing_time_samp = ms_to_samps(mixing_time_ms, fs)
+    crop_end_samp = ms_to_samps(crop_end_ms, fs)
+    trunc_rir = rir[mixing_time_samp:-crop_end_samp]
+    trunc_approx_rir = approx_rir[mixing_time_samp:-crop_end_samp]
+    # get subband T60s
+    true_t60, _, _, _, fitted_edc = get_edc_params(trunc_rir, n_slopes, fs)
+    est_t60, _, _, _, fitted_approx_edc = get_edc_params(
+        trunc_approx_rir, n_slopes, fs)
+    # filter into subbands for EDF
+    filtered_rir = octave_filtering(trunc_rir, fs, band_centre_hz)
+    filtered_approx_rir = octave_filtering(trunc_approx_rir, fs,
+                                           band_centre_hz)
+
+    if plot_edc:
+        num_bands = len(band_centre_hz)
+        fig, ax = plt.subplots(num_bands, 1, figsize=(6, 12))
+        time = np.linspace(0, (len(trunc_rir) - 1) / fs, len(trunc_rir))
+
+        for k in range(num_bands):
+
+            true_edf = np.flipud(
+                np.cumsum(np.flipud(filtered_rir[:, k]**2), axis=-1))
+            approx_edf = np.flipud(
+                np.cumsum(np.flipud(filtered_approx_rir[:, k]**2), axis=-1))
+
+            ax[k].plot(time, db(true_edf, is_squared=True), label='True EDF')
+            ax[k].plot(time[:fitted_edc.shape[-1]],
+                       db(fitted_edc[k, :], is_squared=True),
+                       label='DecayFitNet, True')
+            ax[k].plot(time[:len(approx_edf)],
+                       db(approx_edf, is_squared=True),
+                       label='Synth EDF')
+            ax[k].plot(time[:fitted_approx_edc.shape[-1]],
+                       db(fitted_approx_edc[k, :], is_squared=True),
+                       label='DecayFitNet, Approx')
+
+            ax[k].set_title(f'{band_centre_hz[k]: .0f} Hz')
+
+        fig.subplots_adjust(hspace=0.7)
+        ax[0].legend(loc="upper right")
+
+    return true_t60, est_t60
+
+
+def get_amps_for_rir(rir: NDArray,
+                     common_decay_times: NDArray,
+                     band_centre_hz: ArrayLike,
+                     fs: float,
+                     mixing_time_ms: float = 20.0,
+                     leave_out_ms: float = 10.0) -> NDArray:
+    """
+    Get subband amplitude estimates using least squares method
+    Args:
+        rir (NDArray): RIR of size n_rirs x ir_len
+        common_decay_times (ArrayLike): common decay times for the decay kernels, of size n_rir x n_slopes x n_bands
+        band_centre_hz (ArrayLike): filter frequencies
+        fs (float): sampling rate
+        mixing_time_ms (float): truncate RIR before this time
+        leave_out_ms (float): leave out last few samples in calculating the energy envelope
+    Returns:
+        NDArray: subband amplitudes of shape n_bands x 1 x n_slopes
+    """
+    # crop the RIR upto the mixing time
+    mixing_time_samp = ms_to_samps(mixing_time_ms, fs)
+    trunc_rir = rir[mixing_time_samp:]
+    if len(trunc_rir) % 2 != 0:
+        trunc_rir = rir[mixing_time_samp + 1:]
+    # filter and get the amplitudes for each band
+    filtered_rir = octave_filtering(trunc_rir, fs, band_centre_hz)
+
+    # estimated amplitudes with least squares
+    estimated_amps = calculate_amplitudes_least_squares(
+        common_decay_times,
+        fs,
+        filtered_rir,
+        band_centre_hz,
+        leave_out_ms=leave_out_ms)
+    # n_bands should be the first axis
+    estimated_amps = np.moveaxis(estimated_amps, -1, 0)
+    return estimated_amps
+
+
 def amplitudes_to_initial_level(
         decay_times: NDArray,
         amplitudes: NDArray,
@@ -132,7 +240,7 @@ def amplitudes_to_initial_level(
     f_bands = octave_bands(end_freq=max_freq)
 
     ir_octave_filter = octave_filtering(impulse, fs, f_bands, get_filter=True)
-    # the input inpulse will not be used in this case actually, get_filter argument is just a quick fix
+    # the input impulse will not be used in this case actually, get_filter argument is just a quick fix
     band_energy = np.sum(ir_octave_filter**2, axis=0)
     band_energy = np.tile(band_energy[:, np.newaxis], (1, n_slopes))
 
@@ -148,27 +256,3 @@ def amplitudes_to_initial_level(
     # the ir_len factor is due to the normalization in schroederInt (in DecayFitNet)
 
     return level
-
-
-def calculate_energy_envelope(sig: ArrayLike, fs: float,
-                              smooth_time_ms: float) -> ArrayLike:
-    """
-    Calculate the energy envelope (broadband EDC) of a RIR
-    Args:
-        sig (ArrayLike): 1D RIR signal
-        fs (float): sampling rate
-        smooth_time_ms (float): smoothing window length in ms, 
-                                longer window leads to more smoothing
-    """
-    staps = ms_to_samps(smooth_time_ms / 2, fs)
-    odd_win_len = 2 * staps - 1
-    # normalised smoothing window
-    bs = np.hanning(odd_win_len) / np.sum(np.hanning(odd_win_len))
-    # zero-pad signal on either side
-    padded_signal = np.concatenate((np.zeros(staps), sig**2, np.zeros(staps)),
-                                   axis=0)
-    # smooth signal by convolving with window
-    smoothed_signal = fftconvolve(bs, padded_signal)
-    env = np.real(np.sqrt(smoothed_signal))
-    env = env[odd_win_len + np.arange(len(sig))]
-    return env

src/diff_gfdn/config/config.py
@@ -4,8 +4,9 @@ from enum import Enum
 from typing import List, Optional
 
 import numpy as np
-from pydantic import BaseModel, computed_field, ConfigDict, Field, model_validator
+from pydantic import BaseModel, computed_field, ConfigDict, Field, field_validator, model_validator
 import sympy as sp
+import torch
 
 from ..utils import ms_to_samps
 
@@ -95,7 +96,16 @@ class TrainerConfig(BaseModel):
     reduced_pole_radius: float = Field(
         default=1.0)  # Default value, to be set dynamically
 
-    # Validator for the 'reduced_pole_radius' field
+    # validator for training on GPU
+    @field_validator('device', mode='after')
+    @classmethod
+    def validate_training_device(cls, value):
+        """Validate GPU, if it is used for training"""
+        if value == 'cuda':
+            assert torch.cuda.is_available(
+            ), "CUDA is not available for training"
+
+    # validator for the 'reduced_pole_radius' field
     @model_validator(mode='after')
     @classmethod
     def calculate_reduced_pole_radius(cls, model):

src/diff_gfdn/feedback_loop.py
@@ -157,7 +157,7 @@ class FeedbackLoop(nn.Module):
             num_groups (int): number of groups in the GFDN
             num_delay_lines_per_group (int): number of delay lines in each group
             delays (List): delay line lengths in samples
-            gains (List): delay line absorption gains
+            gains (List): delay line absorption gains / filters
             use_absorption_filters (bool): whether the delay line gains are gains or filters
             coupling_matrix_type (CouplingMatrixType): scalar or filter coupling
             coupling_matrix_order (optional, int): order of the PU filter coupling matrix
@@ -215,6 +215,7 @@ class FeedbackLoop(nn.Module):
                                self.num_delay_lines_per_group) - 1) /
                                   np.sqrt(self.num_delay_lines_per_group))
             if self.coupling_matrix_type == CouplingMatrixType.SCALAR:
+
                 # Nroom choose 2 rotation angles for getting an Nroom x Nroom unitary coupling matrix
                 # these are randomly distributed between 0 and pi/4
                 # self.alpha = nn.Parameter(

src/diff_gfdn/gain_filters.py
@@ -36,6 +36,7 @@ class SVF:
             self.G = 1.0
         else:
             self.G = db2lin(self.G_db)
+        # assert (self.G <= 1.0), "gain cannot be greater than unity!"
 
         if self.filter_type == "lowpass":
             m = torch.cat(
@@ -96,8 +97,8 @@ class SVF:
                 "The filter type not specified or not in the list. Using the given mixing coefficents."
             )
         self.m_LP = m[0]
-        self.m_HP = m[1]
-        self.m_BP = m[2]
+        self.m_BP = m[1]
+        self.m_HP = m[2]
 
 
 @dataclass
@@ -146,7 +147,6 @@ class BiquadCascade:
                        2] = (cur_svf.cutoff_frequency**2 -
                              2 * cur_svf.resonance * cur_svf.cutoff_frequency +
                              1) * compress_pole_factor**2
-
         return BiquadCascade(num_sos, num_coeffs, den_coeffs)
 
 
@@ -300,11 +300,13 @@ class ScaledSoftPlus(nn.Module):
         """
         super().__init__()
         self.soft_plus = SoftPlus()
+        self.sigmoid = Sigmoid()
         self.lower_limit = lower_limit
         self.upper_limit = upper_limit
 
     def forward(self, x: torch.Tensor):
         """Scaled softplus function to get values between lowr_lmit and upper limit"""
+
         return self.lower_limit + (self.upper_limit -
                                    self.lower_limit) * self.soft_plus(x) / (
                                        1 + self.soft_plus(x))
@@ -324,6 +326,26 @@ class TanSigmoid(nn.Module):
     def forward(self, x: torch.Tensor):
         """Tan-sigmoid ensures positive output for SVF frequency"""
         return torch.tan(np.pi * self.sigmoid(x) * 0.5)
+
+
+class InvertedReLU(nn.Module):
+    """ReLU activation between min_value (negative) and 0"""
+
+    def __init__(self, min_value: float):
+        super().__init__()
+        assert min_value < 0.0
+        self.min_value = min_value
+
+    def forward(self, x):
+        linear_part = torch.where((x > self.min_value) & (x < 0), x,
+                                  torch.tensor(0.0, device=x.device))
+
+        # Apply saturation
+        return torch.where(
+            x >= 0, torch.tensor(0.0, device=x.device),
+            torch.where(x <= self.min_value,
+                        torch.tensor(self.min_value, device=x.device),
+                        linear_part))
 
 
 #######################################MLP-RELATED###########################################
@@ -558,9 +580,9 @@ class SVF_from_MLP(nn.Module):
 
         self.sos_filter = SOSFilter(self.num_biquads, device=self.device)
         # constraints on PEQ resonance
-        self.scaled_sigmoid = ScaledSigmoid(lower_limit=1e-6, upper_limit=1.0)
+        self.scaled_res = ScaledSigmoid(lower_limit=1e-6, upper_limit=1.0)
         # constraints on PEQ gains
-        self.soft_plus = ScaledSoftPlus(lower_limit=-6, upper_limit=6)
+        self.scaled_gains = ScaledSigmoid(lower_limit=-6, upper_limit=6)
 
     def forward(self, x: Dict) -> torch.tensor:
         """
@@ -597,9 +619,9 @@ class SVF_from_MLP(nn.Module):
 
         # always ensure that the filter parameters are constrained
         reshape_size = (self.batch_size, self.num_groups, self.num_biquads)
-        self.svf_params[..., 0] = self.scaled_sigmoid(
+        self.svf_params[..., 0] = self.scaled_res(
             self.svf_params[..., 0].view(-1)).view(reshape_size)
-        self.svf_params[..., 1] = self.soft_plus(
+        self.svf_params[..., 1] = self.scaled_gains(
             self.svf_params[..., 1].view(-1)).view(reshape_size)
 
         # initialise empty filters
@@ -657,10 +679,10 @@ class SVF_from_MLP(nn.Module):
     def get_param_dict(self) -> Dict:
         """Return the parameters as a dict"""
         param_np = {}
-        self.svf_params[..., 0] = self.scaled_sigmoid(
+        self.svf_params[..., 0] = self.scaled_res(
             self.svf_params[..., 0].view(-1)).view(self.num_groups,
                                                    self.num_biquads)
-        self.svf_params[..., 1] = self.soft_plus(
+        self.svf_params[..., 1] = self.scaled_gains(
             self.svf_params[..., 1].view(-1)).view(self.num_groups,
                                                    self.num_biquads)
         param_np['svf_params'] = self.svf_params.squeeze().cpu().numpy()
@@ -684,6 +706,7 @@ class Gains_from_MLP(nn.Module):
         num_neurons: int,
         encoding_type: FeatureEncodingType,
         position_type: str = "output_gains",
+        device: Optional[torch.device] = 'cpu',
     ):
         """
         Train the MLP to get scalar gains for each delay line
@@ -706,6 +729,7 @@ class Gains_from_MLP(nn.Module):
         self.num_delay_lines = self.num_groups * self.num_delay_lines_per_group
         self.position_type = position_type
         self.encoding_type = encoding_type
+        self.device = device
 
         if self.encoding_type == FeatureEncodingType.SINE:
             # if we were feeding the spatial coordinates directly, then the

src/diff_gfdn/plot.py
@@ -1,15 +1,18 @@
+from pathlib import Path
 from typing import List, Optional, Tuple
 
+from IPython import display
 from matplotlib import animation
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
 from scipy.fft import rfftfreq
 from scipy.signal import freqz, sosfreqz
+from slope2noise.slope2noise.utils import octave_filtering
 import torch
 
 from .losses import get_edr_from_stft, get_stft_torch
-from .utils import db
+from .utils import db, ms_to_samps
 
 
 def plot_t60_filter_response(
@@ -56,7 +59,7 @@ def plot_t60_filter_response(
         plt.legend([line0[0], line1[0]], ["Measured", "GEQ fit"])
     plt.xlabel('Frequency(Hz)')
     plt.ylabel('Magnitude (dB)')
-    plt.ylim([-10, 5])
+    plt.ylim([-30, 5])
     plt.tight_layout()
 
 
@@ -302,4 +305,86 @@ def animate_coupled_feedback_matrix(
                         bottom=0.1)  # Fine-tune margins
     if save_path is not None:
         ani.save(save_path, writer="pillow", fps=2, dpi=100)
+    plt.show()
+
+
+def plot_subband_edc(h_true: ArrayLike,
+                     h_approx: List[ArrayLike],
+                     fs: float,
+                     band_centre_hz: ArrayLike,
+                     pos_to_investigate: List,
+                     mixing_time_ms: float = 20.0,
+                     crop_end_ms: float = 5.0,
+                     save_path: Optional[str] = None):
+    """
+    Plot true and synthesised EDC curves for each frequency band, as a function of epoch number
+    Args:
+        h_true (ArrayLike): true (desired) RIR at a particular position
+        h_approx (List[ArrayLike]): synthesized RIRs, one for each epoch
+        fs (float): sampling rate
+        band_centre_hz (ArrayLike): centre frequencies of the octave filters
+        pos_to_investigate (List): cartesian coordinate of position where RIR was measured
+        mixing_time_ms (float): truncate RIR before this time
+        crop_end_ms (float): truncate last few samples of RIR
+        save_path (optional, str): where to save figure
+    """
+    mixing_time_samp = ms_to_samps(mixing_time_ms, fs)
+    crop_end_samp = ms_to_samps(crop_end_ms, fs)
+
+    trunc_true_ir = h_true[mixing_time_samp:-crop_end_samp]
+    filtered_true_ir = octave_filtering(trunc_true_ir, fs, band_centre_hz)
+    time = np.linspace(0, (len(trunc_true_ir) - 1) / fs, len(trunc_true_ir))
+
+    num_bands = len(band_centre_hz)
+    time = np.linspace(0, (len(trunc_true_ir) - 1) / fs, len(trunc_true_ir))
+    fig, ax = plt.subplots(num_bands, 1, figsize=(6, 12))
+    fig.subplots_adjust(hspace=0.7)
+    leg = []
+
+    num_epochs = len(h_approx)
+    for epoch in range(num_epochs):
+        approx_ir = h_approx[epoch]
+        trunc_approx_ir = approx_ir[mixing_time_samp:mixing_time_samp +
+                                    len(trunc_true_ir)]
+        filtered_approx_ir = octave_filtering(trunc_approx_ir, fs,
+                                              band_centre_hz)
+        leg.append(f'Epoch = {epoch}')
+
+        for k in range(num_bands):
+            if epoch == 0:
+                true_edf = np.flipud(
+                    np.cumsum(np.flipud(filtered_true_ir[:, k]**2), axis=-1))
+                ax[k].plot(time,
+                           db(true_edf, is_squared=True),
+                           label='True EDF')
+
+            synth_edf = np.flipud(
+                np.cumsum(np.flipud(filtered_approx_ir[:, k]**2), axis=-1))
+            ax[k].plot(time,
+                       db(synth_edf, is_squared=True),
+                       label=f'Epoch={epoch}')
+            ax[k].set_title(f'{band_centre_hz[k]: .0f} Hz')
+            ax[k].set_ylim([-100, 0])
+
+        display.display(fig)  # Display the updated figure
+        display.clear_output(
+            wait=True)  # Clear the previous output to keep updates in place
+
+    # Collect handles and labels from all axes
+    handles, labels = [], []
+    for handle, label in zip(*ax[-1].get_legend_handles_labels()):
+        handles.append(handle)
+        labels.append(label)
+    fig.legend(handles,
+               labels,
+               loc="upper right",
+               ncol=1,
+               frameon=False,
+               bbox_to_anchor=(1.2, 0.5))
+    fig.suptitle(
+        'Truncated EDF at position ' +
+        f'{pos_to_investigate[0]: .2f}, {pos_to_investigate[1]: .2f}, {pos_to_investigate[2]: .2f} m'
+    )
+    if save_path is not None:
+        fig.savefig(Path(save_path).resolve())
     plt.show()

src/diff_gfdn/solver.py
@@ -39,7 +39,8 @@ def convert_common_slopes_rir_to_room_dataset(
     room_start_coords = rir_data.room_start_coords
     source_locs = rir_data.source_locs
     receiver_locs = rir_data.receiver_locs
-    common_decay_times = np.array(rir_data.t_vals)
+    common_decay_times = np.array(rir_data.t_vals).T
+    band_centre_hz = np.array(rir_data.f_bands)
     amplitudes = rir_data.a_vals
     rirs = rir_data.rir
     sample_rate = rir_data.sample_rate
@@ -54,6 +55,7 @@ def convert_common_slopes_rir_to_room_dataset(
         room_dims=room_dims,
         room_start_coord=room_start_coords,
         amplitudes=amplitudes,
+        band_centre_hz=band_centre_hz,
         nfft=num_freq_bins,
     )
 

src/diff_gfdn/trainer.py
@@ -93,6 +93,14 @@ class Trainer:
                 'lr':
                 trainer_config.io_lr
             },
+            {
+                'params': [
+                    param for name, param in self.net.named_parameters()
+                    if 'output_svf_params' in name
+                ],
+                'lr':
+                trainer_config.io_lr
+            },
             # Add more groups as needed
         ]
 
@@ -100,7 +108,7 @@ class Trainer:
         other_params = [
             param for name, param in self.net.named_parameters()
             if not ('feedback_loop.alpha' in name or 'input_gains' in name
-                    or 'output_gains' in name)
+                    or 'output_gains' in name or 'output_svf_params' in name)
         ]
 
         # Add the other parameters with a learning rate of 0.01
@@ -377,16 +385,6 @@ class SinglePosTrainer(Trainer):
         for name, prm in self.net.named_parameters():
             if name in ('input_gains', 'output_gains'):
                 prm.data.copy_(torch.div(prm.data, torch.pow(energyH, 1 / 4)))
-
-            if name == 'output_svf_params':
-                # to scale the magnitude response, the numerator coefficients
-                # in each biquad cascade need to be scaled
-                scaling_factor = torch.pow(torch.pow(energyH, 1 / 4),
-                                           1.0 / float(self.net.num_biquads))
-                # m_i^{LP}, m_i{HP}, m_i^{BP} need to be divided by the scaling factor for all i
-                tmp_filters = prm.data.clone()
-                tmp_filters[..., 2:] /= scaling_factor
-                prm.data.copy_(tmp_filters)
 
     @torch.no_grad()
     def save_ir(self,