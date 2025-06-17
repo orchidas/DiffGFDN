1aaa0c4529a7ab2cbaf1532605ff62b219b1a6d8
orchidas <orchisama.das@gmail.com>
2025-06-17 19:24:56 +0100

    added notebook mplementing McKenzie's method for SRIR interpolation, enabled plotting of receiver positions in test set only in solver.py

--- src/diff_gfdn/dataloader.py
+++ src/diff_gfdn/dataloader.py
@@ -286,6 +286,16 @@ class RoomDataset(ABC):
         """Frequency bins in Hz"""
         return rfftfreq(self.num_freq_bins, d=1.0 / self.sample_rate)
 
+    def find_rec_idx_in_room_dataset(self, rec_pos_list: NDArray) -> List:
+        """Indices of the receivers in the dataset associated with the list of receiver positions"""
+
+        # Compute Euclidean distance between each array in array_list_np and every row in matrix
+        distances = np.linalg.norm(self.receiver_position[:, None, :] -
+                                   rec_pos_list,
+                                   axis=2)
+        indices = np.argmin(distances, axis=0)
+        return indices
+
     def early_late_split(self, win_len_ms: float = 5.0):
         """Split the RIRs into early and late response based on mixing time"""
         mixing_time_samps = ms_to_samps(self.mixing_time_ms, self.sample_rate)
